9416880217105882f08dd00e38ebcd4e5cba298c
Marek Jaczewski <jaczewski147@gmail.com>
2020-10-22 21:20:24 +0200

    add home module"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { ProductService } from './services/product.service';
 
 @NgModule({
   declarations: [
